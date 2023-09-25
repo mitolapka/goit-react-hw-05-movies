832d6e6f0a376ee46cb34e5cbbf793f24d552a6d
mitolapka <124922415+mitolapka@users.noreply.github.com>
2023-09-25 12:30:24 +0300

    vinally-version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Movies from './Movies';
