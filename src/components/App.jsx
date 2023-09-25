3fc8ab76501f0a1d885beeeb5b5af7e5a3c96583
mitolapka <124922415+mitolapka@users.noreply.github.com>
2023-09-25 14:01:17 +0300

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Movies from './Movies';
@@ -8,14 +8,16 @@ import MovieDetails from './MovieDetails';
 
 export const App = () => {
   return (
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} />
-        </Routes>
-      </div>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+      </Routes>
+    </div>
   );
 };
+
+// index.js
 
