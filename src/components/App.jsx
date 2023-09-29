0b577f62cb9366803ed271655a66f3ebc75be602
mitolapka <124922415+mitolapka@users.noreply.github.com>
2023-09-29 15:27:42 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 const Home = React.lazy(() => import('../pages/Home'));
 const Movies = React.lazy(() => import('../pages/Movies'));
@@ -8,15 +8,14 @@ const MovieDetails = React.lazy(() => import('../pages/MovieDetails'));
 export const App = () => {
   return (
     <div>
-        <Header />
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/movies/:movieId" element={<MovieDetails />} />
-          </Routes>
-        </Suspense>
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
-
